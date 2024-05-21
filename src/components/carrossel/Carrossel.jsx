079756fe059df3a31d65d301fde24eae71c890c5
Silvio <silviopln@hotmail.com>
2024-05-21 00:29:38 -0300

    Carrossel atualizado

--- src/components/carrossel/Carrossel.jsx
+++ src/components/carrossel/Carrossel.jsx
@@ -1,58 +1,84 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import "./carrossel.css"
-import logo from "../../assets/logo.svg"
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
-import imageRicardo from "../../assets/Ricardo.jpg"
-import imageIgor from "../../assets/Igor.PNG"
-import imageThomas from "../../assets/Thomas.PNG"
-import imageDennis from "../../assets/Dennis.PNG"
-import imageSilvio from "../../assets/Silvio.PNG"
+import React, { useState } from 'react';
+import imageRicardo from "../../assets/Ricardo.jpg";
+import imageIgor from "../../assets/Igor.PNG";
+import imageThomas from "../../assets/Thomas.PNG";
+import imageDennis from "../../assets/Dennis.PNG";
+import imageSilvio from "../../assets/Silvio.PNG";
+import imageLucas from "../../assets/Lucas.PNG";
+import "./carrossel.css";
 
 const Carrossel = () => {
+  const teamMembers = [
+    { name: "Silvio", image: imageSilvio },
+    { name: "Ricardo", image: imageRicardo },
+    { name: "Igor", image: imageIgor },
+    { name: "Thomas", image: imageThomas },
+    { name: "Dennis", image: imageDennis },
+    { name: "Lucas", image: imageLucas },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % teamMembers.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? teamMembers.length - 1 : prevIndex - 1
+    );
+  };
+
+  const getVisibleItems = () => {
+    const visibleItems = [];
+    for (let i = 0; i < 3; i++) {
+      const index = (currentIndex + i) % teamMembers.length;
+      visibleItems.push(teamMembers[index]);
+    }
+    return visibleItems;
+  };
+
+  const visibleItems = getVisibleItems();
+
   return (
-    <div class="work">
+    <div className="work">
       <h2>Equipe</h2>
-      <div class="work--lockup">
-        <ul class="slider">
-          <li class="slider--item slider--item-left">
-            <div class="slider--item-image">
-              <img class="img" src={imageSilvio} alt="Silvio" />
+      <div className="work--lockup">
+        <div className="slider">
+          {visibleItems.map((member, index) => (
+            <div
+              key={index}
+              className={`slider--item ${index === 1 ? "slider--item-center" : ""
+                }`}
+            >
+              <div className="slider--item-image">
+                <img className="img" src={member.image} alt={member.name} />
+              </div>
+              <p className="slider--item-title">{member.name}</p>
             </div>
-            <p class="slider--item-title">Silvio</p>
-          </li>
-          <li class="slider--item slider--item-left">
-            <div class="slider--item-image">
-              <img class="img" src={imageRicardo} alt="Ricardo" />
-            </div>
-            <p class="slider--item-title">Ricardo</p>
-          </li>
-          <li class="slider--item slider--item-center">
-            <div class="slider--item-image">
-              <img class="img" src={imageIgor} alt="Igor" />
-            </div>
-            <p class="slider--item-title">Igor</p>
-          </li>
-          <li class="slider--item slider--item-right">
-            <div class="slider--item-image">
-              <img class="img" src={imageThomas} alt="Thomas" />
-            </div>
-            <p class="slider--item-title">Thomas</p>
-          </li>
-          <li class="slider--item slider--item-right1">
-            <div class="slider--item-image">
-              <img class="img" src={imageDennis} alt="Dennis" />
-            </div>
-            <p class="slider--item-title">Dennis</p>
-          </li>
-        </ul>
-        <div class="slider--prev">
+          ))}
         </div>
-        <div class="slider--next">
+        <div className="slider--controls">
+          <button onClick={handlePrev} className="slider--prev">
+            <img className="back-forward"
+              width="50"
+              height="50"
+              src="https://img.icons8.com/ios/50/back--v1.png"
+              alt="back--v1"
+            />
+          </button>
+          <button onClick={handleNext} className="slider--next">
+            <img className="back-forward"
+              width="50"
+              height="50"
+              src="https://img.icons8.com/ios/50/forward--v1.png"
+              alt="forward--v1"
+            />
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Carrossel
+export default Carrossel;
