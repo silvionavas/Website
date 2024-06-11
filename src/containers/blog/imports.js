8eee1046127e71303996605acecf2f7a382a7961
Silvio <silviopln@hotmail.com>
2024-06-11 14:40:56 -0300

    Mudanças da pre release

--- src/containers/blog/imports.js
+++ src/containers/blog/imports.js
@@ -2,7 +2,7 @@ import blog01 from "../../assets/infografico.jpg";
 import blog02 from "../../assets/TutorialSuno00.png";
 import blog03 from "../../assets/edicaoMusical.jpg";
 import blog04 from "../../assets/image04.jpg";
-import blog05 from "../../assets/image01.jpg";
+import blog05 from "../../assets/blog05.jpg";
 
 export {
     blog01,
